657c29825b7df7b39a39fd661cc63bc42cdd13fa
SushiBar1111 <fachry.altair18@gmail.com>
2024-05-25 23:11:35 +0700

    penambahan url untuk JWT token

--- studyhubdjango/urls.py
+++ studyhubdjango/urls.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('register.urls')),
     path('login/', include('login.urls')),
     path('interest/', include('userInterest.urls')),
     path('filter/', include('filter.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('search/', include('search.urls')),
     path('updateProfile/', include('updateProfile.urls')),
-    path('profile/', include('Profile.urls'))
+    path('profile/', include('Profile.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
 
 if settings.DEBUG: # buat saat development aja nanti pas mau deployment diganti ke path sesuai server yg digunain.
