147c3c1b42d93320f5fbeb01e19b423c2cbe7dee
SushiBar1111 <fachry.altair18@gmail.com>
2024-05-25 17:55:24 +0700

    finished update profile feature

--- updateProfile/serializer.py
+++ updateProfile/serializer.py
@@ -4,4 +4,16 @@ from Profile.models import UserProfile
 class updateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = [ 'first_name', 'last_name', 'gender', 'location', 'role']
+        extra_kwargs = {'id':{'read_only':True}, # extra kwargs ini buat validasi serializer biar user ga harus update semua data mereka sekaligus
+                        'email':{'required':False, 'allow_blank':True},
+                        'first_name':{'required':False, 'allow_blank':True},
+                        'last_name':{'required':False, 'allow_blank':True},
+                        'gender':{'required':False, 'allow_blank':True},
+                        'location':{'required':False, 'allow_blank':True},
+                        'role':{'required':False, 'allow_blank':True},
+                        'bio':{'required':False, 'allow_blank':True},
+                        'academicLevel':{'required':False, 'allow_blank':True},
+                        'studyPlace':{'required':False, 'allow_blank':True},
+                        'learningType':{'required':False, 'allow_blank':True},
+                        }
+        fields = [ 'id','email','first_name', 'last_name', 'gender', 'location', 'role', 'bio', 'academicLevel', 'studyPlace', 'learningType']
--- updateProfile/views.py
+++ updateProfile/views.py
@@ -1,15 +1,22 @@
 from rest_framework.views import APIView
-from .serializer import updateProfileSerializer
 from rest_framework.response import Response
+from rest_framework import status
 from Profile.models import UserProfile
+from .serializer import updateProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UpdateProfile(APIView):
+    
+
     def patch(self, request):
-        user = request.user
-        profile = user.profile
         
-        serializer = updateProfileSerializer(profile, data=request.data)
+        id = request.data.get('id') # ngambil ID dari user yg request
+        try:
+            user = UserProfile.objects.get(id=id)
+        except UserProfile.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = updateProfileSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(status=200)
-        return Response(status=400)
+            return Response({'message': 'Profile updated successfully'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
