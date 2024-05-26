6ed8503e3084dac8841d71ba54d0053ac8abc59e
SushiBar1111 <fachry.altair18@gmail.com>
2024-05-26 23:10:38 +0700

    updated model interest

--- interest/models.py
+++ interest/models.py
@@ -6,4 +6,6 @@ class Matkul(models.Model):
         ('Coding', 'Coding'), ('Law', 'Law'), ('Accounting', 'Accounting'),
     )
     matkul = models.CharField(max_length=10, choices=MATKUL_CHOICES)
-
+    
+    def __str__(self):
+        return self.matkul
