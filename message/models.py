ea39ae2e48f5b3b0d68bd440f0519ed2778a3e24
SushiBar1111 <fachry.altair18@gmail.com>
2024-05-26 22:47:45 +0700

    fitur message. belom selesai

---
+++ message/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from Profile.models import UserProfile
+
+class Convo(models.Model): # buat naro user mana aja yg punya convo ini
+    user1 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='convo_user1')
+    user2 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='convo_user2')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+         return f"Conversation between {self.user1} and {self.user2}"
+
+class Message(models.Model):
+     Convo = models.ForeignKey(Convo, on_delete=models.CASCADE)
+     sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+     text = models.TextField()
+
+     def __str__(self):
+          return f"Message from {self.sender} in conversation {self.Convo}"
