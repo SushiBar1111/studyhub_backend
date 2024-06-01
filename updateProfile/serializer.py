2aa5c6c4c37b43236d0e36b42228fee631998c1b
SushiBar1111 <fachry.altair18@gmail.com>
2024-06-02 01:26:34 +0700

    update serializer

--- updateProfile/serializer.py
+++ updateProfile/serializer.py
@@ -7,13 +7,13 @@ class updateProfileSerializer(serializers.ModelSerializer):
         extra_kwargs = {'id':{'read_only':True}, # extra kwargs ini buat validasi serializer biar user ga harus update semua data mereka sekaligus
                         'email':{'required':False, 'allow_blank':True},
                         'first_name':{'required':False, 'allow_blank':True},
-                        'last_name':{'required':False, 'allow_blank':True},
                         'gender':{'required':False, 'allow_blank':True},
                         'location':{'required':False, 'allow_blank':True},
+                        'matkul':{'required':False, 'allow_blank':True},
                         'role':{'required':False, 'allow_blank':True},
                         'bio':{'required':False, 'allow_blank':True},
                         'academicLevel':{'required':False, 'allow_blank':True},
                         'studyPlace':{'required':False, 'allow_blank':True},
                         'learningType':{'required':False, 'allow_blank':True},
                         }
-        fields = [ 'id','email','first_name', 'gender', 'location', 'role', 'bio', 'academicLevel', 'studyPlace', 'learningType']
+        fields = [ 'id','email','first_name', 'gender', 'location', 'matkul', 'role', 'bio', 'academicLevel', 'studyPlace', 'learningType']
