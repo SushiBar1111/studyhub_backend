a8e56365b9ea45a125cba05c926c493aacf4d06c
SushiBar1111 <fachry.altair18@gmail.com>
2024-06-22 07:15:40 +0700

    update serializer

--- Profile/serializer.py
+++ Profile/serializer.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import UserProfile
-
+import base64
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = [ 'first_name', 'email', 'gender', 'matkul', 'location', 'role', 'bio', 'learningType', 'studyPlace', 'academicLevel']
+        fields = [ 'first_name', 'email', 'gender', 'matkul', 'location', 'role', 'bio', 'learningType', 'studyPlace', 'academicLevel', 'profilePicture']
+    def getProfilePicture(self, obj):
+        if obj.profilePicture:
+            return base64.b64encode(obj.profilePicture.file.read()).decode('utf-8')
+        return None
