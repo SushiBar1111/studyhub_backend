b4e695375b5761be0a4d7147af0a9aa0b95a3145
SushiBar1111 <fachry.altair18@gmail.com>
2024-06-22 14:22:05 +0700

    update project root file

--- studyhubdjango/settings.py
+++ studyhubdjango/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'search',
     'like',
     'message',
+    'api',
     
 ]
 
--- studyhubdjango/urls.py
+++ studyhubdjango/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from message.views import MessageCreateView, MessageListView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('register.urls')),
@@ -14,7 +15,15 @@ urlpatterns = [
     path('profile/', include('Profile.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('like/', include('like.urls'))
+    path('api/token/validate/', include('api.urls')),
+    path('like/', include('like.urls')),
+    path('profileImage/', include('updateProfile.urls')),
+    path('liked/', include('like.urls')),
+    path('match/', include('like.urls')),
+    path('convo/', include('message.urls')),
+    path('message/', MessageCreateView.as_view(), name='create_message'),
+    path('message/list/', MessageListView.as_view(), name='message_list'),
+    path('getConvoId/', include('message.urls'))
 ]
 
 if settings.DEBUG: # buat saat development aja nanti pas mau deployment diganti ke path sesuai server yg digunain.
